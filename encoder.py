93b90dd3643548c4d1bf5127b58eef48a56d92a4
dvdg6566 <czhdaniel@gmail.com>
2021-01-01 15:20:29 +0800

    fixed profile-less pic

--- encoder.py
+++ encoder.py
@@ -1,5 +1,5 @@
 import base64
 
-with open("assets/coloured/9_small.png", "rb") as image_file:
+with open("assets/pfp-edited.png", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
     print(encoded_string)
